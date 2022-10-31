4cc5254b0464c9daf524275c2b7c18c37105b760
Moise-code <moisewowe@gmail.com>
2022-10-31 13:28:15 +0200

    adding the main javascipt file

--- index.js
+++ index.js
@@ -1,158 +1,137 @@
-// const awesomeBooks = document.querySelector('#awesome-books');
+import Store from './modules/store.js';
+import UI from './modules/ui.js';
+import { DateTime } from './modules/luxon.js';
+
+const store = new Store();
+const ui = new UI();
+
+let dt = DateTime.now();
+dt = dt.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+const date = document.querySelector('.date');
+date.innerHTML = dt;
+
 class Book {
-    constructor() {
-      this.awesomeBooks = document.querySelector('#awesome-books');
-      this.formSelector = document.querySelector('form');
-      this.titleSelector = document.getElementById('title');
-      this.authorSelector = document.getElementById('author');
-      this.allBtnSelector = this.awesomeBooks.querySelectorAll('.btn');
-  
-      this.books = JSON.parse(localStorage.getItem('booksLocalStorage')) || [];
-      this.title = localStorage.getItem('currentTitle') || null;
-      this.author = localStorage.getItem('currentAuthor') || null;
-    }
-  
-    /* Display an added book to the UI */
-    display() {
-      this.awesomeBooks.innerHTML += `
-    
-        <li id="${this.id}">
-        <h2>"${this.title}"  by   ${this.author}</h2>
-        <button class="btn button-shadow">remove</button>
-        </li>
-    
-        `;
-    }
-  
-    /*
-  Filter out a removed book from an array of books contianed in each list item
-  - add new array of books (with one less book) to the local storage
-  */
-    remove(id) {
-      this.books = this.books.filter((book) => book.id !== id);
-      localStorage.setItem('booksLocalStorage', JSON.stringify(this.books));
-      this.removeBorderFromBooks();
-    }
-  
-    /*
-  Initialize remove button event listeners
-  */
-    removeDom() {
-      this.allBtnSelector = this.awesomeBooks.querySelectorAll('.btn');
-      this.allBtnSelector.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-          const parent = e.target.parentNode;
-          this.remove(parent.id);
-          parent.remove();
-        });
-      });
-    }
-  
-    /*
-  Add a book object to the books array
-  - display the book on the UI
-  - set booksLocalStorage variable for preserving books array during reload
-  - clear currentTitle and currentAuthor variables so that those will
-    not be showing up after a book object has already been added
-  - initialize event listeners for the buttons created when adding new books
-  */
-  
-    add(bookObj) {
-      if (this.books.length === 0) {
-        document.getElementById('no-book').style.display = 'none';
-        // document.getElementById('added-book').style.textAlign = "center";
-      }
-      this.display();
-      this.books.push(bookObj);
-      localStorage.setItem('booksLocalStorage', JSON.stringify(this.books));
-      localStorage.setItem('currentTitle', '');
-      localStorage.setItem('currentAuthor', '');
-      this.removeDom();
-      this.addBorderToBooks();
-    }
-  
-    /*
-  Create a Book object and add it to the array
-  */
-    formSubmitted() {
-      this.formSelector.onsubmit = (e) => {
-        e.preventDefault();
-        const { title, author } = e.target;
-        const book = new Book();
-        book.title = title.value;
-        book.author = author.value;
-        book.id = Date.now().toString();
-  
-        const bookObj = {
-          title: title.value,
-          author: author.value,
-          id: Date.now().toString(),
-        };
-        // this.add(new Book(title.value, author.value, Date.now().toString()));
-        book.add(bookObj);
-        e.target.title.value = '';
-        e.target.author.value = '';
-      };
-    }
-  
-    /*
-  Listen to the keyup event of the title input box and add it
-  to the local storage to use it when reloading
-  */
-    addEventListenerForTitle() {
-      this.titleSelector.addEventListener('keyup', (e) => {
-        e.preventDefault();
-        localStorage.setItem('currentTitle', e.target.value);
-      });
-    }
-  
-    /*
-  Listen to the keyup event of the author input box and add it
-  to the local storage to use it when reloading
-  */
-    addEventListenerForAuthor() {
-      this.authorSelector.addEventListener('keyup', (e) => {
-        e.preventDefault();
-        localStorage.setItem('currentAuthor', e.target.value);
-      });
-    }
-  
-    populateBooksOnload() {
-      this.books.forEach((book) => {
-        const b = new Book();
-        b.title = book.title;
-        b.author = book.author;
-        b.id = book.id;
-        b.display();
-      });
-  
-      document.getElementById('title').value = this.title;
-      document.getElementById('author').value = this.author;
-      this.removeDom(this.awesomeBooks);
-      this.removeBorderFromBooks();
-    }
-  
-    removeBorderFromBooks() {
-      if (this.books.length === 0) {
-        this.awesomeBooks.style.border = 'none';
-        document.getElementById('no-book').style.display = 'block';
-      } else {
-        document.getElementById('no-book').style.display = 'none';
-      }
-    }
-  
-    addBorderToBooks() {
-      this.awesomeBooks.style.border = '2px solid black';
-    }
+  constructor(title, author, id = Math.floor(Math.random() * 1000000000000000)) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
   }
-  
-  /*
-  Display books, title and author variables when the window loads
-  */
-  
-  const book = new Book();
-  book.formSubmitted();
-  book.addEventListenerForTitle();
-  book.addEventListenerForAuthor();
-  window.onload = book.populateBooksOnload();
-  
+}
+
+// EVENT TO DISPLAY BOOKS
+document.addEventListener('DOMContentLoaded', ui.showBooks);
+// EVENT TO ADD A BOOK
+document.querySelector('#book-card').addEventListener('submit', (e) => {
+  e.preventDefault();
+  // get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  if (title !== '' && author !== '') {
+    const book = new Book(title, author);
+    // CALL THE OTHER CLASSES TO PASS AND DISPLAY THE BOOKS TO UI
+    ui.addBookToList(book);
+    ui.clearFields();
+    store.addBooks(book);
+  } else {
+    alert('Please fill inputs');
+  }
+});
+// EVENT DELETE
+document.querySelector('.bookDisplay').addEventListener('click', (e) => {
+  if (e.target.className === 'remove') {
+    const id = e.target.previousElementSibling.innerText;
+    store.deleteBookStorage(id);
+    ui.deleteBook(e.target);
+  }
+});
+
+const form = document.querySelector('#book-card');
+const awesome = document.querySelector('.awesomeBooks');
+const bookDisplay = document.querySelector('.bookDisplay');
+const list = document.querySelector('.lists');
+const addNew = document.querySelector('.addNew');
+const contact = document.querySelector('.contact');
+const contactUs = document.querySelector('.contactUs');
+
+list.addEventListener('click', () => {
+  awesome.style.display = 'block';
+  bookDisplay.style.display = 'block';
+  form.style.display = 'none';
+  contactUs.style.display = 'none';
+});
+
+addNew.addEventListener('click', () => {
+  form.style.display = 'block';
+  awesome.style.display = 'none';
+  bookDisplay.style.display = 'none';
+  contactUs.style.display = 'none';
+});
+
+contact.addEventListener('click', () => {
+  contactUs.innerHTML = `
+  <h2 class="contactUsHeader">Contact Information</h2>
+  <p class="contactUsParagraph">Do you have any question, or you just want to say "Hi"?
+      <br>
+      You can reach out to us!
+  </p>
+  <div class="contactDiv">
+      <ul class="contactUsUl">
+          <li>- &nbsp; Our email address jaamanu@gmail.com</li>
+          <li>- &nbsp; Our email address famini@gmail.com </li>
+          <li>- &nbsp; Our phone numbers +270730732316</li>
+          <li>- &nbsp; Our house address 23 chicago ave capetown South Africa</li>
+      </ul>
+  </div>
+  `;
+  contactUs.style.display = 'block';
+  form.style.display = 'none';
+  awesome.style.display = 'none';
+  bookDisplay.style.display = 'none';
+});
+
+// const title = document.querySelector('#title');
+// const author = document.querySelector('#author');
+// const form = document.getElementById('book-card');
+// const library = document.getElementById('library');
+// let book;
+// let bookList = JSON.parse(localStorage.getItem('bookList')) || [];
+// function addBook() {
+//   book = {
+//     title: title.value,
+//     author: author.value,
+//     id: Math.floor(Math.random() * 1000000),
+//   };
+//   bookList.push(book);
+//   localStorage.setItem('bookList', JSON.stringify(bookList));
+// }
+// function removeBook(id) {
+//   bookList = bookList.filter((books) => books.id !== id);
+//   localStorage.setItem('bookList', JSON.stringify(bookList));
+// }
+// function populate(book) {
+//   const row = document.createElement('tr');
+//   const bookTitle = document.createElement('td');
+//   const bookAuthor = document.createElement('td');
+//   const removeBtn = document.createElement('button');
+//   bookTitle.innerText = book.title;
+//   bookAuthor.innerText = book.author;
+//   removeBtn.innerText = 'Remove';
+//   row.append(bookTitle, bookAuthor, removeBtn);
+//   library.append(row);
+//   removeBtn.addEventListener('click', () => {
+//     removeBtn.parentElement.remove();
+//     removeBook(book.id);
+//   });
+// }
+// bookList.forEach(populate);
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   if (title.value !== '' && author.value !== '') {
+//     addBook();
+//     populate(book);
+//     form.reset();
+//   } else {
+//     alert('Please enter a title and author');
+//   }
+// });
